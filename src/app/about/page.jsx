b807c7b0ff1477fe7914097fefc9a04fc69c45e7
Nolan332933 <nolanshelby81@gmail.com>
2024-03-31 17:07:35 +0700

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        In addition to our extensive movie database, we also offer a platform
+        for movie lovers to connect and share their thoughts on the latest
+        releases. Our community section includes a forum where you can discuss
+        your favorite films with like-minded individuals and read reviews and
+        ratings from other users. We also have a section dedicated to movie news
+        and trailers, keeping you updated with the latest happenings in the
+        world of cinema. Thank you for visiting our website and we hope you
+        enjoy your time browsing through our movie database. If you have any
+        feedback or suggestions, please feel free to contact us. We are always
+        looking for ways to improve and enhance the user experience on our
+        website. Happy browsing!
+      </p>
+    </div>
+  );
+}
